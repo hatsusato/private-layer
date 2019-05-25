2d2466727d92669e7657bc4013e6e784820459e3
hatsusato <knightspm5@gmail.com>
2019-05-25 15:54:02 +0900

    Add initial layers

---
+++ layers.el
@@ -0,0 +1,16 @@
+(configuration-layer/declare-layers '(;; auto-completion
+                                      ;; better-defaults
+                                      emacs-lisp
+                                      ;; git
+                                      helm
+                                      ;; markdown
+                                      multiple-cursors
+                                      ;; org
+                                      ;; (shell :variables
+                                      ;;        shell-default-height 30
+                                      ;;        shell-default-position 'bottom)
+                                      ;; spell-checking
+                                      ;; syntax-checking
+                                      treemacs
+                                      ;; version-control
+                                      ))
