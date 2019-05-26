c27fd0d86e91791d9125b1b07724246bef7b3c6b
hatsusato <knightspm5@gmail.com>
2019-05-26 13:27:02 +0900

    Refactor indentation

--- layers.el
+++ layers.el
@@ -1,22 +1,23 @@
-(configuration-layer/declare-layers '(auto-completion
-                                      (c-c++ :variables
-                                             c-c++-enable-google-style t
-                                             c-c++-enable-clang-support t
-                                             c-c++-default-mode-for-headers 'c++-mode)
-                                      emacs-lisp
-                                      git
-                                      helm
-                                      html
-                                      javascript
-                                      markdown
-                                      multiple-cursors
-                                      ocaml
-                                      org
-                                      (shell :variables
-                                             shell-default-height 30
-                                             shell-default-position 'bottom)
-                                      (syntax-checking :variables
-                                                       syntax-checking-enable-by-default nil)
-                                      treemacs
-                                      version-control
-                                      ))
+(configuration-layer/declare-layers
+ '(auto-completion
+   (c-c++ :variables
+          c-c++-enable-google-style t
+          c-c++-enable-clang-support t
+          c-c++-default-mode-for-headers 'c++-mode)
+   emacs-lisp
+   git
+   helm
+   html
+   javascript
+   markdown
+   multiple-cursors
+   ocaml
+   org
+   (shell :variables
+          shell-default-height 30
+          shell-default-position 'bottom)
+   (syntax-checking :variables
+                    syntax-checking-enable-by-default nil)
+   treemacs
+   version-control
+   ))
