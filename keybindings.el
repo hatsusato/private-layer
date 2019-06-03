de9166ad65d31f923997da9d12ee23f9fce39892
hatsusato <knightspm5@gmail.com>
2019-06-03 16:29:04 +0900

    Use henkan key to enable mozc mode

--- keybindings.el
+++ keybindings.el
@@ -1,3 +1,4 @@
 (bind-key* "<hiragana-katakana>" 'hatsusato/enable-mozc-mode)
+(bind-key* "<henkan>" 'hatsusato/enable-mozc-mode)
 (bind-key* "<muhenkan>" 'hatsusato/disable-mozc-mode)
 (bind-key* "<zenkaku-hankaku>" 'hatsusato/toggle-mozc-mode)
