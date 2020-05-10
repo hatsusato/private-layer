b1bff049fd3d544f51bc26c49852445bb0a67da7
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-05-10 14:50:09 +0900

    Comment language layers out

--- layers.el
+++ layers.el
@@ -1,33 +1,33 @@
 (configuration-layer/declare-layers
  '(auto-completion
-   (c-c++ :variables
-          c-c++-adopt-subprojects t
-          c-c++-backend 'lsp-ccls
-          ;; c-c++-default-mode-for-headers 'c++-mode
-          c-c++-enable-clang-format-on-save t
-          c-c++-enable-google-style t
-          c-c++-enable-organize-includes-on-save t
-          c-c++-lsp-cache-dir (concat spacemacs-cache-directory "lsp-ccls")
-          c-c++-lsp-sem-highlight-method 'font-lock
-          c-c++-lsp-sem-highlight-rainbow t)
-   (cmake :variables
-          cmake-enable-cmake-ide-support t)
+   ;; (c-c++ :variables
+   ;;        c-c++-adopt-subprojects t
+   ;;        c-c++-backend 'lsp-ccls
+   ;;        ;; c-c++-default-mode-for-headers 'c++-mode
+   ;;        c-c++-enable-clang-format-on-save t
+   ;;        c-c++-enable-google-style t
+   ;;        c-c++-enable-organize-includes-on-save t
+   ;;        c-c++-lsp-cache-dir (concat spacemacs-cache-directory "lsp-ccls")
+   ;;        c-c++-lsp-sem-highlight-method 'font-lock
+   ;;        c-c++-lsp-sem-highlight-rainbow t)
+   ;; (cmake :variables
+   ;;        cmake-enable-cmake-ide-support t)
    dap
    emacs-lisp
    git
    helm
-   html
-   javascript
+   ;; html
+   ;; javascript
    (lsp :variables
         lsp-ui-sideline-enable nil)
-   latex
+   ;; latex
    markdown
    multiple-cursors
    neotree
-   ocaml
+   ;; ocaml
    org
-   python
-   rust
+   ;; python
+   ;; rust
    semantic
    (shell :variables
           shell-default-height 30
