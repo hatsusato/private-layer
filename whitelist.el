702e53cd945f5ea394fbc4443dfddf41470080dd
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-06-06 05:35:44 +0900

    Add whitelist.el

---
+++ whitelist.el
@@ -0,0 +1,19 @@
+(defconst hatsusato/layers-whitelist
+  '(
+    ;; auto-completion
+    ;; better-defaults
+    emacs-lisp
+    ;; git
+    helm
+    ;; lsp
+    ;; markdown
+    multiple-cursors
+    ;; org
+    ;; (shell :variables
+    ;;        shell-default-height 30
+    ;;        shell-default-position 'bottom)
+    ;; spell-checking
+    ;; syntax-checking
+    treemacs
+    ;; version-control
+    ))
